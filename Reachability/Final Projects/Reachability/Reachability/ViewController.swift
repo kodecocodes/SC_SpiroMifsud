392026975382b42c6ec14758c0b2754ac4dd57aa
James Taylor <jamestaylortx@gmail.com>
2018-04-29 11:54:26 -0500

    add setReachabilityNotifier back

--- Reachability/Final Projects/Reachability/Reachability/ViewController.swift
+++ Reachability/Final Projects/Reachability/Reachability/ViewController.swift
@@ -36,6 +36,7 @@ class ViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     displayReachableAlert()
+    //setReachabilityNotifier()
   }
   
   private func displayReachableAlert() {
